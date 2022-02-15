b5426b412816c4613330d15f54b38d9c120dc097
sarath s <sarath.s@webandcrafts.in>
2022-02-15 18:00:23 +0530

    latest

--- src/script.js
+++ src/script.js
@@ -16,21 +16,22 @@ const gui = new dat.GUI();
 const loader = new GLTFLoader();
 
 
-
+gsap.registerPlugin(ScrollTrigger);
 
 loader.load(
   "/3d/s9_mini_drone/scene.gltf",
+  // "/gltf file _ ref.glb",
   function (gltf) {
     var drone = gltf.scene;
-    drone.scale.set(1, 1, 1);
+    drone.scale.set(.5, .5, .5);
 
-    drone.position.x = 33; // once rescaled, position the model where needed
-    drone.position.y = 16;
-    drone.position.z = -40;
+    drone.position.x = 0; // once rescaled, position the model where needed
+    drone.position.y = 3;
+    drone.position.z = -10;
 
-    drone.rotation.x = 0.62; // once rescaled, position the model where needed
-    drone.rotation.y = -1;
-    drone.rotation.z = 0.22;
+    drone.rotation.x = 0.2; // once rescaled, position the model where needed
+    drone.rotation.y = 0;
+    drone.rotation.z = 0;
 
     gui.add(gltf.scene.position, "x").min(-100).max(100).step(0.01);
     gui.add(gltf.scene.position, "y").min(-100).max(100).step(0.01);
@@ -42,12 +43,12 @@ loader.load(
 
     scene.add(drone);
 
-    gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
+    gsap.registerPlugin(ScrollTrigger);
 
     ScrollTrigger.defaults({
       immediateRender: false,
       ease: "power1.inOut",
-      scrub: true,
+      // scrub: true,
     });
 
     let drone_anim = gsap.timeline();
@@ -66,7 +67,7 @@ loader.load(
       },
     });
     drone_anim.to(drone.rotation, {
-      y: 2.4,
+      y: -1,
       scrollTrigger: {
         trigger: ".section-two",
         scrub: 1.5,
@@ -74,17 +75,26 @@ loader.load(
         end: "top bottom",
       },
     });
-
-
-    // Slide 2
-
-    drone_anim.to(camera.position, {
-      x: -0.1,
+    drone_anim.to(drone.position, {
+      y: 1,
+      x: 0,
+      z: -10,
       scrollTrigger: {
-        trigger: ".section-two",
-
-        start: "top bottom",
-        end: "top top",
+        trigger: ".section-three",
+        scrub: 0.6,
+        endTrigger: ".section-four",
+        end: "top bottom",
+      },
+    });
+    drone_anim.to(drone.rotation, {
+      y: -3,
+      x: 1,
+      z: 0,
+      scrollTrigger: {
+        trigger: ".section-four",
+        scrub: 0.6,
+        endTrigger: ".section-four",
+        end: "top bottom",
       },
     });
 
@@ -126,41 +136,41 @@ loader.load(
 );
 
 
-loader.load(
-    "/3d/city/scene.gltf",
-    function (gltf) {
-      var table = gltf.scene;
-      table.scale.set(.5, .5, .5);
+// loader.load(
+//     "/3d/city/scene.gltf",
+//     function (gltf) {
+//       var table = gltf.scene;
+//       table.scale.set(.5, .5, .5);
   
-      table.position.x = 0; // once rescaled, position the model where needed
-      table.position.y = 0;
-      table.position.z = 0;
+//       table.position.x = 0; // once rescaled, position the model where needed
+//       table.position.y = 0;
+//       table.position.z = 0;
   
-      table.rotation.x = 0; // once rescaled, position the model where needed
-      table.rotation.y = 0;
-      table.rotation.z = 0;
+//       table.rotation.x = 0; // once rescaled, position the model where needed
+//       table.rotation.y = 0;
+//       table.rotation.z = 0;
   
       
   
   
-      const tableFolder = gui.addFolder('city position')
-      tableFolder.add(gltf.scene.position, "x").min(-100).max(100).step(0.01);
-      tableFolder.add(gltf.scene.position, "y").min(-100).max(100).step(0.01);
-      tableFolder.add(gltf.scene.position, "z").min(-100).max(100).step(0.01);
-      tableFolder.add(gltf.scene.rotation, "y").min(-100).max(100).step(0.01);
+//       const tableFolder = gui.addFolder('city position')
+//       tableFolder.add(gltf.scene.position, "x").min(-100).max(100).step(0.01);
+//       tableFolder.add(gltf.scene.position, "y").min(-100).max(100).step(0.01);
+//       tableFolder.add(gltf.scene.position, "z").min(-100).max(100).step(0.01);
+//       tableFolder.add(gltf.scene.rotation, "y").min(-100).max(100).step(0.01);
 
 
-      scene.add(table);
+//       scene.add(table);
   
-      let table_anim = gsap.timeline();
+//       let table_anim = gsap.timeline();
   
      
-    },
-    undefined,
-    function (error) {
-      console.error(error);
-    }
-  );
+//     },
+//     undefined,
+//     function (error) {
+//       console.error(error);
+//     }
+//   );
 
 
 // Canvas
@@ -186,19 +196,38 @@ const scene = new THREE.Scene();
 
 // Lights
 
-const hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 4);
-scene.add(hemiLight)
+// const hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 4);
+// scene.add(hemiLight)
 
-const lightSpot = new THREE.SpotLight(0xffa95c,4);
-lightSpot.position.set(-50,50,50);
-lightSpot.castShadow = true;
-scene.add( lightSpot );
+// const lightSpot = new THREE.SpotLight(0xffa95c,4);
+// lightSpot.position.set(-50,50,50);
+// lightSpot.castShadow = true;
+// scene.add( lightSpot );
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1);
-pointLight.position.set(10, 10, 3);
-pointLight.intensity = 7;
+const pointLight = new THREE.PointLight(0xff8787, 2, 50);
+pointLight.position.set(-3, 5, -5);
 
 
+
+// Full Height
+
+gsap.to(pointLight.position, {
+  x: 4.36,
+  z: -7,
+  scrollTrigger: {
+    trigger: ".section-two",
+    endTrigger: ".section-four",
+    start: "top top",
+    scrub: 2.5,
+    markers: true,
+    end: "top bottom",
+  },
+});
+
+
+
+const sphereSize = 0.5;
+const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
 
 
 const lightFolder1 = gui.addFolder('point light 1')
@@ -209,13 +238,20 @@ lightFolder1.add(pointLight, "intensity").min(-10).max(10).step(0.01);
 
 lightFolder1.open()
 scene.add(pointLight);
+scene.add( pointLightHelper );
 
 // Lights2
 
-const pointLight2 = new THREE.PointLight(0xffffff, 2);
-pointLight2.position.set(-2, 3, 1);
+const pointLight2 = new THREE.PointLight(0x3a9ce1, 2, 50);
+pointLight2.position.set(4, 2, -4);
 pointLight2.intensity = 17;
+
+const sphereSize1 = 0.5;
+const pointLightHelper1 = new THREE.PointLightHelper( pointLight2, sphereSize1 );
 scene.add(pointLight2);
+scene.add( pointLightHelper1 );
+
+
 
 const lightFolder2 = gui.addFolder('point light 2')
 lightFolder2.add(pointLight2.position, "x").min(-10).max(10).step(0.01);
@@ -267,8 +303,8 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 camera.position.x = 0;
-camera.position.y = 0;
-camera.position.z = 2;
+camera.position.y = 3;
+camera.position.z = 0;
 scene.add(camera);
 
 
