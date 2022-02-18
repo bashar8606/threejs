ffba07d9fb1dbb3985af4661ce54be526e0fba07
sarath s <sarath.s@webandcrafts.in>
2022-02-18 15:50:23 +0530

    class toggle body

--- src/script.js
+++ src/script.js
@@ -6,28 +6,36 @@ import * as dat from "dat.gui";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 //texture
 
-// const textureLoader = new THREE.TextureLoader()
-
-// const normalTexture = textureLoader.load('/normalMap.jpg')
+const textureLoader = new THREE.TextureLoader();
 // Debug
 const gui = new dat.GUI();
 const loader = new GLTFLoader();
-
-
+let mixer;
 gsap.registerPlugin(ScrollTrigger);
 
 loader.load(
-  "/3d/s9_mini_drone/scene.gltf",
+  // "/3d/s9_mini_drone/scene.gltf",
+  "/3d/super/scene.gltf",
   // "/gltf file _ ref.glb",
   function (gltf) {
     var drone = gltf.scene;
-    drone.scale.set(.5, .5, .5);
 
-    drone.position.x = 0; // once rescaled, position the model where needed
-    drone.position.y = 3;
-    drone.position.z = -10;
+    var animations = gltf.animations;
+
+    drone.traverse((o) => {
+      if (o.isMesh) {
+        // o.material = new THREE.MeshBasicMaterial({map: textureLoader.load(`/3d/super/textures/IOS_Superman_Mat_baseColor.png`)});
+        // o.material1 = new THREE.MeshBasicMaterial({map: textureLoader.load(`/3d/super/textures/IOS_Superman_Mat_metallicRoughness.png`)});
+      }
+    });
+    drone.scale.set(0.15, 0.15, 0.15);
+    drone.position.set(0, 9, -40);
+    // drone.position.x = 0;
+    // drone.position.y = 3;
+    // drone.position.z = -10;
 
     drone.rotation.x = 0.2; // once rescaled, position the model where needed
     drone.rotation.y = 0;
@@ -37,12 +45,16 @@ loader.load(
     gui.add(gltf.scene.position, "y").min(-100).max(100).step(0.01);
     gui.add(gltf.scene.position, "z").min(-100).max(100).step(0.01);
 
-    gui.add(gltf.scene.rotation, 'x').min(-100).max(100).step(.01)
-    gui.add(gltf.scene.rotation, 'y').min(-100).max(100).step(.01)
-    gui.add(gltf.scene.rotation, 'z').min(-100).max(100).step(.01)
+    gui.add(gltf.scene.rotation, "x").min(-100).max(100).step(0.01);
+    gui.add(gltf.scene.rotation, "y").min(-100).max(100).step(0.01);
+    gui.add(gltf.scene.rotation, "z").min(-100).max(100).step(0.01);
 
     scene.add(drone);
+    mixer = new THREE.AnimationMixer(gltf.scene);
 
+    mixer.clipAction(gltf.animations[0]).play();
+
+    tick();
     gsap.registerPlugin(ScrollTrigger);
 
     ScrollTrigger.defaults({
@@ -56,13 +68,12 @@ loader.load(
     // Full Height
 
     drone_anim.to(drone.position, {
-      y: 2.4,
-      x: 4.36,
-      z: -7,
+      y: 26,
+      z: -38,
       scrollTrigger: {
         trigger: ".section-two",
         scrub: 0.6,
-        endTrigger: ".section-four",
+        endTrigger: ".section-three",
         end: "top bottom",
       },
     });
@@ -75,59 +86,7 @@ loader.load(
         end: "top bottom",
       },
     });
-    drone_anim.to(drone.position, {
-      y: 1,
-      x: 0,
-      z: -10,
-      scrollTrigger: {
-        trigger: ".section-three",
-        scrub: 0.6,
-        endTrigger: ".section-four",
-        end: "top bottom",
-      },
-    });
-    drone_anim.to(drone.rotation, {
-      y: -3,
-      x: 1,
-      z: 0,
-      scrollTrigger: {
-        trigger: ".section-four",
-        scrub: 0.6,
-        endTrigger: ".section-four",
-        end: "top bottom",
-      },
-    });
-
-    // Slide 3
-
-    //  drone_anim.to(scene.rotation, { z: 1.6, scrollTrigger: {
-
-    //      trigger: ".section-three",
-
-    //      start: "top bottom",
-    //      end: "top top",
-
-    // }})
-
-    // // Slide 4 - The problem child
-
-    //  drone_anim.to(scene.rotation, { z: 0.02, y: 3.1, scrollTrigger: {
-
-    //      trigger: ".section-four",
-
-    //      start: "top bottom",
-    //      end: "top top",
-
-    // }})
-
-    //  drone_anim.to(camera.position, { x: 0.16, scrollTrigger: {
-
-    //      trigger: ".section-four",
-
-    //      start: "top top",
-    //      end: "bottom top",
-
-    // }})
+ 
   },
   undefined,
   function (error) {
@@ -135,43 +94,36 @@ loader.load(
   }
 );
 
-
 // loader.load(
 //     "/3d/city/scene.gltf",
 //     function (gltf) {
 //       var table = gltf.scene;
 //       table.scale.set(.5, .5, .5);
-  
+
 //       table.position.x = 0; // once rescaled, position the model where needed
 //       table.position.y = 0;
 //       table.position.z = 0;
-  
+
 //       table.rotation.x = 0; // once rescaled, position the model where needed
 //       table.rotation.y = 0;
 //       table.rotation.z = 0;
-  
-      
-  
-  
+
 //       const tableFolder = gui.addFolder('city position')
 //       tableFolder.add(gltf.scene.position, "x").min(-100).max(100).step(0.01);
 //       tableFolder.add(gltf.scene.position, "y").min(-100).max(100).step(0.01);
 //       tableFolder.add(gltf.scene.position, "z").min(-100).max(100).step(0.01);
 //       tableFolder.add(gltf.scene.rotation, "y").min(-100).max(100).step(0.01);
 
-
 //       scene.add(table);
-  
+
 //       let table_anim = gsap.timeline();
-  
-     
+
 //     },
 //     undefined,
 //     function (error) {
 //       console.error(error);
 //     }
 //   );
-
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -204,10 +156,9 @@ const scene = new THREE.Scene();
 // lightSpot.castShadow = true;
 // scene.add( lightSpot );
 
-const pointLight = new THREE.PointLight(0xff8787, 2, 50);
+const pointLight = new THREE.PointLight(0xffffff, 2, 50);
+// const pointLight = new THREE.PointLight(0xff8787, 2, 50);
 pointLight.position.set(-3, 5, -5);
-
-
 
 // Full Height
 
@@ -224,21 +175,18 @@ gsap.to(pointLight.position, {
   },
 });
 
-
-
 const sphereSize = 0.5;
-const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
+const pointLightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
 
+const lightFolder1 = gui.addFolder("point light 1");
+lightFolder1.add(pointLight.position, "x").min(-100).max(100).step(0.01);
+lightFolder1.add(pointLight.position, "y").min(-100).max(100).step(0.01);
+lightFolder1.add(pointLight.position, "z").min(-100).max(100).step(0.01);
+lightFolder1.add(pointLight, "intensity").min(-100).max(100).step(0.01);
 
-const lightFolder1 = gui.addFolder('point light 1')
-lightFolder1.add(pointLight.position, "x").min(-10).max(10).step(0.01);
-lightFolder1.add(pointLight.position, "y").min(-10).max(10).step(0.01);
-lightFolder1.add(pointLight.position, "z").min(-10).max(10).step(0.01);
-lightFolder1.add(pointLight, "intensity").min(-10).max(10).step(0.01);
-
-lightFolder1.open()
+lightFolder1.open();
 scene.add(pointLight);
-scene.add( pointLightHelper );
+scene.add(pointLightHelper);
 
 // Lights2
 
@@ -247,18 +195,15 @@ pointLight2.position.set(4, 2, -4);
 pointLight2.intensity = 17;
 
 const sphereSize1 = 0.5;
-const pointLightHelper1 = new THREE.PointLightHelper( pointLight2, sphereSize1 );
+const pointLightHelper1 = new THREE.PointLightHelper(pointLight2, sphereSize1);
 scene.add(pointLight2);
-scene.add( pointLightHelper1 );
+scene.add(pointLightHelper1);
 
-
-
-const lightFolder2 = gui.addFolder('point light 2')
+const lightFolder2 = gui.addFolder("point light 2");
 lightFolder2.add(pointLight2.position, "x").min(-10).max(10).step(0.01);
 lightFolder2.add(pointLight2.position, "y").min(-10).max(10).step(0.01);
 lightFolder2.add(pointLight2.position, "z").min(-10).max(10).step(0.01);
 lightFolder2.add(pointLight2, "intensity").min(-10).max(10).step(0.01);
-
 
 // const materialGui = gui.addFolder('material color')
 
@@ -307,19 +252,15 @@ camera.position.y = 3;
 camera.position.z = 0;
 scene.add(camera);
 
+const cameraPosition = gui.addFolder("camera position");
+cameraPosition.add(camera.position, "x").min(-100).max(100).step(0.01);
+cameraPosition.add(camera.position, "y").min(-100).max(100).step(0.01);
+cameraPosition.add(camera.position, "z").min(-100).max(100).step(0.01);
 
-const cameraPosition = gui.addFolder('camera position')
-cameraPosition.add(camera.position, 'x').min(-100).max(100).step(.01)
-cameraPosition.add(camera.position, 'y').min(-100).max(100).step(.01)
-cameraPosition.add(camera.position, 'z').min(-100).max(100).step(.01)
-
-
-      
-const cameraRotation = gui.addFolder('camera Rotation')
-cameraRotation.add(camera.rotation, 'x').min(-100).max(100).step(.01)
-cameraRotation.add(camera.rotation, 'y').min(-100).max(100).step(.01)
+const cameraRotation = gui.addFolder("camera Rotation");
+cameraRotation.add(camera.rotation, "x").min(-100).max(100).step(0.01);
+cameraRotation.add(camera.rotation, "y").min(-100).max(100).step(0.01);
 // cameraRotation.add(camera.rotation, 'z').min(-100).max(100).step(.01)
-
 
 // Controls
 // const controls = new OrbitControls(camera, canvas)
@@ -335,14 +276,18 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
+// renderer.physicallyCorrectLights = true;
 /**
  * Animate
  */
 
 const clock = new THREE.Clock();
+function tick() {
+  const delta = clock.getDelta();
 
-const tick = () => {
+  mixer.update(delta);
+
+  requestAnimationFrame(tick);
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
@@ -355,7 +300,7 @@ const tick = () => {
   renderer.render(scene, camera);
 
   // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
+  // window.requestAnimationFrame(tick);
+}
 
-tick();
+// tick();
